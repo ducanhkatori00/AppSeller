7c269c850ec082397341f9d3c265d6cac83a6b16
DESKTOP-04IF1D6\ducan <ducanhkatori00@gmail.com>
2021-05-31 11:10:14 +0700

    feat: demo

---
+++ AppSeller.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AppSeller.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ AppSeller.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AppSeller.Models
+{
+    public class Class1
+    {
+    }
+}
